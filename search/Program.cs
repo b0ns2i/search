831b9e3e2786f16c2e5acd54721d03bb1c28c04e
b0ns2i <colistop@mail.ru>
2023-09-25 21:43:50 +1000

    ДЗ1

--- search/Program.cs
+++ search/Program.cs
@@ -11,12 +11,15 @@ namespace search
     {
         static void Main(string[] args)
         {
+           
+            Searcher searcher = new Searcher();
+           
 
-            
+
+
+
             string path = Console.ReadLine();
             int d = 0;
-            string substring = "*.";
-            int indexOfSubstring = substring.IndexOf(substring);
             path = path.Replace("*.", "#.");
 
 
@@ -38,7 +41,7 @@ namespace search
                 string subpath = path.Substring(path.IndexOf('*') + 1);
                 string[] folder_and_file = subpath.Trim().Split('\\');
                 folder_and_file[2] = folder_and_file[2].Replace("#.", "*.");
-                Basic_search(main_path_directoryInfo, folder_and_file);
+                searcher.Basic_search(main_path_directoryInfo, folder_and_file);
                
             }
             if (d == 2)
@@ -47,7 +50,7 @@ namespace search
                 string[] folder_and_file = subpath.Trim().Split('\\');
                 folder_and_file[2] = folder_and_file[2].Replace("#.", "*.");
                 Console.WriteLine();
-                Recursive_search(main_path_directoryInfo, folder_and_file);
+                searcher.Recursive_search(main_path_directoryInfo, folder_and_file);
             }
 
             Console.WriteLine("Программа завершила работу!");
@@ -55,109 +58,7 @@ namespace search
            Console.ReadLine();
         }
 
-        static void Basic_search(DirectoryInfo main_path, string[] folder_and_file)
-        {
-            DirectoryInfo[] directoryInfo = null;
-            DirectoryInfo[] subDirectoryInfo = null;
-            FileInfo[] filesInfo = null;
-
-            try
-            {
-                directoryInfo = main_path.GetDirectories();
-            }
-            catch { }
-
-            for (int i = 0; i < directoryInfo.Length; i++)
-            {
-
-                try
-                {
-                    subDirectoryInfo = directoryInfo[i].GetDirectories();
-                }
-                catch { }
-
-                for (int j = 0; j < subDirectoryInfo.Length; j++)
-                {
-
-                
-
-                     if (Convert.ToString(subDirectoryInfo[j]) == folder_and_file[1])
-                     {
-                          try
-                          {
-                            filesInfo = subDirectoryInfo[j].GetFiles(folder_and_file[2]);
-                          }
-                          catch { }
-
-                         if (filesInfo != null)
-                         {
-                             for (int k = 0; k < filesInfo.Length; k++)
-                             {
-                                    
-                                 
-
-                                Console.WriteLine("Найден файл: " + filesInfo[k].FullName);
-                             }
-                         }
-
-                     }
-                }
-            }
-
-        } 
-
-        static void Recursive_search(DirectoryInfo main_path, string[] folder_and_file)
-        {
-            DirectoryInfo[] directoryInfo = null;
-            FileInfo[] filesInfo = null;
 
 
-            try
-            {
-                directoryInfo = main_path.GetDirectories();
-            }
-            catch { }
-          
-            if (directoryInfo != null)
-            {
-
-                for (int i = 0; i < directoryInfo.Length; i++ )
-                {
-                    if (Convert.ToString(directoryInfo[i]) == folder_and_file[1])
-                    {
-
-                        //
-                        try
-                        {
-                            filesInfo = directoryInfo[i].GetFiles(folder_and_file[2]);
-                        }
-                        catch { }
-
-                        if (filesInfo != null)
-                        {
-                            for (int k = 0; k < filesInfo.Length; k++)
-                            {
-
-                                Console.WriteLine("Найден файл: " + filesInfo[k].FullName);
-                            }
-                        }
-                        else
-                        {
-
-                            Recursive_search(directoryInfo[i], folder_and_file); 
-                        }
-                    }
-                    else
-                    {
-                        Recursive_search(directoryInfo[i], folder_and_file);
-                    }
-                }
-
-
-               
-                
-            }
-
-        }
     }
 }
---
+++ search/Searcher.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace search
+{
+    public  class Searcher
+    {
+
+
+        public void Basic_search(DirectoryInfo main_path, string[] folder_and_file)
+        {
+            DirectoryInfo[] directoryInfo = null;
+            DirectoryInfo[] subDirectoryInfo = null;
+            FileInfo[] filesInfo = null;
+
+            try
+            {
+                directoryInfo = main_path.GetDirectories();
+            }
+            catch { }
+
+            for (int i = 0; i < directoryInfo.Length; i++)
+            {
+
+                try
+                {
+                    subDirectoryInfo = directoryInfo[i].GetDirectories();
+                }
+                catch { }
+
+                for (int j = 0; j < subDirectoryInfo.Length; j++)
+                {
+
+
+
+                    if (Convert.ToString(subDirectoryInfo[j]) == folder_and_file[1])
+                    {
+                        try
+                        {
+                            filesInfo = subDirectoryInfo[j].GetFiles(folder_and_file[2]);
+                        }
+                        catch { }
+
+                        if (filesInfo != null)
+                        {
+                            for (int k = 0; k < filesInfo.Length; k++)
+                            {
+
+
+
+                                Console.WriteLine("Найден файл: " + filesInfo[k].FullName);
+                            }
+                        }
+
+                    }
+                }
+            }
+
+        }
+
+        public void Recursive_search(DirectoryInfo main_path, string[] folder_and_file)
+        {
+            DirectoryInfo[] directoryInfo = null;
+            FileInfo[] filesInfo = null;
+
+
+            try
+            {
+                directoryInfo = main_path.GetDirectories();
+            }
+            catch { }
+
+            if (directoryInfo != null)
+            {
+
+                for (int i = 0; i < directoryInfo.Length; i++)
+                {
+                    if (Convert.ToString(directoryInfo[i]) == folder_and_file[1])
+                    {
+
+                        //
+                        try
+                        {
+                            filesInfo = directoryInfo[i].GetFiles(folder_and_file[2]);
+                        }
+                        catch { }
+
+                        if (filesInfo != null)
+                        {
+                           
+                                for (int k = 0; k < filesInfo.Length; k++) // 
+                                {
+
+                                    Console.WriteLine("Найден файл: " + filesInfo[k].FullName);
+                                }
+                          
+                         
+                        }
+                        else
+                        {
+
+                            Recursive_search(directoryInfo[i], folder_and_file);
+                        }
+                    }
+                    else
+                    {
+                        Recursive_search(directoryInfo[i], folder_and_file);
+                    }
+                }
+
+
+
+
+            }
+
+        }
+
+
+    }
+}
